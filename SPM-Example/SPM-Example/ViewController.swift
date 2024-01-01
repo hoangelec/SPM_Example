1ed688ff8b58511a44a25933212b2bd8d4b9cb17
Mike Cap <hoangelec@icloud.com>
2024-01-01 16:10:59 +0700

    Initial commit

---
+++ SPM-Example/SPM-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM-Example
+//
+//  Created by Hoang Cap on 01/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
